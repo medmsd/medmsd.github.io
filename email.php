0366244655329e8044058b310f7e19c5821ee30a
Mseddi Mohamed <mseddi.mohamed@iit.ens.tn>
2021-06-24 11:14:24 +0100

    fix email send

--- email.php
+++
@@ -1,6 +0,0 @@
-if(!empty($_POST['subject']) &&  !empty($_POST['body']))
-{
-    $subject=$_POST['subject'];
-    $body=$_POST['body'];
-    window.open('mailto:mseddi.mohamed@iit.ens.tn?subject='.$subject.'&body='.$body);
-}
